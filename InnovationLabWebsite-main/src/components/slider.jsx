b1e8d6e2e1c5683487ca8f3a7e97a9f1b2c81922
Shubham <shubhamyadav45623@gmail.com>
2025-09-22 10:52:28 +0530

    image added to corousel

--- InnovationLabWebsite-main/src/components/slider.jsx
+++ InnovationLabWebsite-main/src/components/slider.jsx
@@ -132,38 +132,21 @@
 
 // export default ImageSlider;
 import '../styles/slider.css'; 
-import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const ImageSlider = () => {
-  const [imageLinks, setImageLinks] = useState([]);
-  const folderId = '126qTT7sPi0MQgFgOttKGwWl9aKyuZ4sJ'; 
-  const apiKey = 'AIzaSyAjTf2BaoCVkvoSkac-zRh4JJI9H2BByQs'; 
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`;
-
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log('Fetched Data:', data);  // Debug API response
-
-        if (data.files) {
-          const links = data.files.map(file => `https://drive.google.com/uc?export=view&id=${file.id}`);
-          setImageLinks(links);
-        } else {
-          console.error('No files found in folder.');
-        }
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }
-    };
-
-    fetchImages();
-  }, [folderId, apiKey]);
+  
+  
+  const imageFiles = [
+    'image1.jpeg',
+    'image2.jpeg',
+    'image3.jpeg',
+    // Add all the file names from your carousel folder
+  ];
+  
+  const images = imageFiles.map(file => `/corousel/${file}`);
 
   const settings = {
     dots: true,
@@ -178,17 +161,13 @@ const ImageSlider = () => {
   return (
     <div className="slider-container">
       <h1 className="gallery-heading">Gallery</h1>
-      {imageLinks.length > 0 ? (
-        <Slider {...settings}>
-          {imageLinks.map((image, index) => (
-            <div key={index} className="slide-wrapper">
-              <img src={image} alt={`Slide ${index + 1}`} className="slider-image" />
-            </div>
-          ))}
-        </Slider>
-      ) : (
-        <p className="loading-text">Loading images...</p>
-      )}
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="slide-wrapper">
+            <img src={image} alt={`Slide ${index + 1}`} className="slider-image" />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
